0598df4be8008d45786d57927f55114529173e13
ajin94 <ajinsanut@gmail.com>
2019-04-05 19:31:06 +0530

    update

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('client/index.html')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('client/projects.html')
+
+
+@app.route('/testimonials')
+def testimonials():
+    return render_template('client/testimonials.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('client/aboutus.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
