e367e26865bb6ecdfdef066d511c75d01a23d622
ajin94 <ajinsanut@gmail.com>
2019-04-08 20:30:16 +0530

    'update'

--- server.py
+++ server.py
@@ -4,22 +4,22 @@ from flask import render_template
 sfapp = Flask(__name__)
 
 
-@app.route('/')
+@sfapp.route('/')
 def index():
     return render_template('client/index.html')
 
 
-@app.route('/projects')
+@sfapp.route('/projects')
 def projects():
     return render_template('client/projects.html')
 
 
-@app.route('/testimonials')
+@sfapp.route('/testimonials')
 def testimonials():
     return render_template('client/testimonials.html')
 
 
-@app.route('/about')
+@sfapp.route('/about')
 def about():
     return render_template('client/aboutus.html')
 
