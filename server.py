f7a8b27828ca9c49703a5b48196561ec41d90ac8
ajin94 <ajinsanut@gmail.com>
2019-04-10 07:54:27 +0530

    'update'

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session
 from flask import render_template
 
 sfapp = Flask(__name__)
@@ -6,7 +6,10 @@ sfapp = Flask(__name__)
 
 @sfapp.route('/')
 def index():
-    return render_template('client/index.html')
+    template_arguments = {
+        "user-name": session.get('user', None)
+    }
+    return render_template('client/index.html', template_data=template_arguments)
 
 
 @sfapp.route('/projects')
@@ -24,4 +27,6 @@ def about():
     return render_template('client/aboutus.html')
 
 if __name__ == '__main__':
+    sfapp.secret_key = '6wfwef6ASDW676w6QDWD6748wd((FD'
+    sfapp.config['SESSION_TYPE'] = 'filesystem'
     sfapp.run(host='0.0.0.0')
