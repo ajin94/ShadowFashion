23d090ace2fed1902141aca035910d584d931eb5
ajin sanu <ajinsanut@gmail.com>
2019-04-25 21:10:53 +0530

    update

--- connections.py
+++ connections.py
@@ -2,7 +2,7 @@ import MySQLdb
 
 
 def get_connection():
-    conn = MySQLdb.connect(host="localhost", user="root", passwd="sh@d0w", port=3306, db="shadowfashion")
+    conn = MySQLdb.connect(host="localhost", user="root", passwd="password", port=3306, db="shadowfashion")
     cursor = conn.cursor()
     return cursor, conn
 
