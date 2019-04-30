45a7fd5db9b2079c79f812c32ce73b5d38cc293e
Ajin Sanu <30728152+ajin94@users.noreply.github.com>
2019-04-30 07:38:48 +0530

    Update server.py

--- server.py
+++ server.py
@@ -185,5 +185,5 @@ def check_uname_duplicate():
         return json.dumps({'status': 'ERROR'})
     return json.dumps({'status': 'OK'})
 
-#if __name__ == "__main__":
-#    sfapp.run()
+if __name__ == "__main__":
+    sfapp.run()
